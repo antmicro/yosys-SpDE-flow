c68329792a9740351d1ad41cf2e3abadc4b0fad7
Grzegorz Latosinski <glatosinski@antmicro.com>
2019-12-19 11:24:02 +0100

    Improved converting script
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- verilogs-to-edifs-yosys.sh
+++ verilogs-to-edifs-yosys.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-mkdir -p ./qlogic-edifs
-mkdir -p ./qlogic-edifs-sdpe
-fd -e v --full-path './qlogic-verilogs/' -x ./yosys -p "read_verilog {}; synth_quicklogic -edif qlogic-edifs/{/.}.edf"
-fd -e edf --full-path './qlogic-edifs/' -x python ./convert-lut-inits.py {} qlogic-edifs-sdpe/{/.}.edf
+SCRIPTDIR=$(dirname "$(realpath $0)")
+
+YOSYS_EXEC=${1%/} # Yosys executable
+VERILOG_INPUTS=${2%/} # directory with verilogs to process
+YOSYS_EDIFS_DIR=${3%/} # directory in which the EDIFS produced by Yosys should be stored
+SPDE_EDIFS_DIR=${4%/} # directory in which the EDIFS produced by Python script for SpDE should be stored
+
+TO_SPDE_CONVERT_SCRIPT=$SCRIPTDIR/convert-lut-inits.py
+
+mkdir -p $YOSYS_EDIFS_DIR
+mkdir -p $SPDE_EDIFS_DIR
+fd -e v --full-path $VERILOG_INPUTS/ -x $YOSYS_EXEC -p "read_verilog {}; synth_quicklogic -edif $YOSYS_EDIFS_DIR/{/.}.edf"
+fd -e edf --full-path $YOSYS_EDIFS_DIR -x python $TO_SPDE_CONVERT_SCRIPT {} $SPDE_EDIFS_DIR/{/.}.edf
